f947bcc4cb16c7b9d028a015c9e75a8a2492ca2a
Christian Pannwitz <ch.von.pa@googlemail.com>
2019-09-17 21:51:52 +0200

    cleaned up blogpost

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -3,7 +3,7 @@ import { MDXRenderer } from "gatsby-plugin-mdx"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/Seo/Seo"
-import { rhythm, scale } from "../utils/typography"
+import css from "./BlogPost.module.css"
 
 import "gitalk/dist/gitalk.css"
 import gitalkConfig from "../utils/gitalk-config"
@@ -26,7 +26,6 @@ class BlogPostTemplate extends React.Component {
   render() {
     const { mdx: post, site } = this.props.data
     const siteTitle = site.siteMetadata.title
-    // const { previous, next } = this.props.pageContext
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -34,65 +33,15 @@ class BlogPostTemplate extends React.Component {
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
-        <article>
-          <header>
-            <h1
-              style={{
-                marginTop: rhythm(1),
-                marginBottom: 0,
-              }}
-            >
-              {post.frontmatter.title}
-            </h1>
-            <p
-              style={{
-                ...scale(-1 / 5),
-                display: `block`,
-                marginBottom: rhythm(1),
-              }}
-            >
-              {post.frontmatter.date}
-            </p>
+        <article className={css.article}>
+          <header className={css.header}>
+            <h1 className={css.title}>{post.frontmatter.title}</h1>
+            <small className={css.date}>{post.frontmatter.date}</small>
           </header>
-          <MDXRenderer title="WHAT">{post.body}</MDXRenderer>
-          {/* <section dangerouslySetInnerHTML={{ __html: post.body }} /> */}
-          <hr
-            style={{
-              marginBottom: rhythm(1),
-            }}
-          />
-          {/* <footer>
-          </footer> */}
+          <MDXRenderer>{post.body}</MDXRenderer>
         </article>
 
         <div id="gitalk-container" />
-
-        {/* <nav>
-          <ul
-            style={{
-              display: `flex`,
-              flexWrap: `wrap`,
-              justifyContent: `space-between`,
-              listStyle: `none`,
-              padding: 0,
-            }}
-          >
-            <li>
-              {previous && (
-                <Link to={previous.fields.slug} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
-        </nav> */}
       </Layout>
     )
   }
